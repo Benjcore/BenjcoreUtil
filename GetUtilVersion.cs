761d29e96945c72cf1c6eb4d9375ec1a52390646
Benj_2005 <>
2022-09-22 07:44:17 +1000

    Bump version to v2.1.1

--- GetUtilVersion.cs
+++ GetUtilVersion.cs
@@ -13,7 +13,7 @@ public static class GetUtilVersion
     /// <summary>
     /// BenjcoreUtil Version as an Int Array.
     /// </summary>
-    public static readonly uint[] VersionAsUIntArray = { 2, 1, 0 };
+    public static readonly uint[] VersionAsUIntArray = { 2, 1, 1 };
 
     /// <summary>
     /// BenjcoreUtil Version as a String.
