0c1ec5f79b5b676cddbcd60306b58b537c180acb
Benj2005 <62969241+Benj2005@users.noreply.github.com>
2022-11-02 03:57:26 +1100

    docs: Improve clarity
    Improve the clarity of documentation comments in IVersion.cs.

--- Versioning/IVersion.cs
+++ Versioning/IVersion.cs
@@ -5,36 +5,46 @@ public interface IVersion
     /// <summary>
     /// Checks if the version is newer than another version.
     /// </summary>
-    /// <param name="input">The version object you wish to compare.</param>
-    /// <returns>Boolean Result</returns>
+    /// <param name="input">The version object you wish to compare to the current.</param>
+    /// <returns>
+    /// A boolean indicating whether or not the current version is newer than the given version.
+    /// </returns>
     bool IsNewerThan(IVersion input);
 
     /// <summary>
     /// Checks if the version is newer than or equal to than another version.
     /// </summary>
-    /// <param name="input">The version object you wish to compare.</param>
-    /// <returns>Boolean Result</returns>
+    /// <param name="input">The version object you wish to compare to the current.</param>
+    /// <returns>
+    /// A boolean indicating whether or not the current version is newer than or equal to the given version.
+    /// </returns>
     bool IsNewerThanOrEqualTo(IVersion input);
 
     /// <summary>
     /// Checks if the version is older than another version.
     /// </summary>
-    /// <param name="input">The version object you wish to compare.</param>
-    /// <returns>Boolean Result</returns>
+    /// <param name="input">The version object you wish to compare to the current.</param>
+    /// <returns>
+    /// A boolean indicating whether or not the current version is older than the given version.
+    /// </returns>
     bool IsOlderThan(IVersion input);
 
     /// <summary>
     /// Checks if the version is older than or equal to than another version.
     /// </summary>
-    /// <param name="input">The version object you wish to compare.</param>
-    /// <returns>Boolean Result</returns>
+    /// <param name="input">The version object you wish to compare to the current.</param>
+    /// <returns>
+    /// A boolean indicating whether or not the current version is older than or equal to the given version.
+    /// </returns>
     bool IsOlderThanOrEqualTo(IVersion input);
 
     /// <summary>
     /// Checks if the version is equal to than another version.
     /// </summary>
-    /// <param name="input">The version object you wish to compare.</param>
-    /// <returns>Boolean Result</returns>
+    /// <param name="input">The version object you wish to compare to the current.</param>
+    /// <returns>
+    /// A boolean indicating whether or not the current version is equal to the given version.
+    /// </returns>
     bool IsEqualTo(IVersion input);
 
     /// <returns>A string representing the version object.</returns>
