2fc7081641a7883b92458ab2b879ebe02e118bb4
Benj2005 <62969241+Benj2005@users.noreply.github.com>
2024-01-24 19:29:48 +1100

    feat: Allow BenjcoreUtil version to be a PreviewVersion The following public fields have also been changed to properties (this should not be a breaking change): - VersionAsSimpleVersion - VersionAsUIntArray - VersionAsString

--- GetUtilVersion.cs
+++ GetUtilVersion.cs
@@ -1,5 +1,6 @@
 ﻿global using System;
 global using System.Collections.Generic;
+using System.Linq;
 using BenjcoreUtil.Versioning;
 #pragma warning disable CS0618
 using Version = BenjcoreUtil.Versioning.Version;
@@ -9,16 +10,36 @@ namespace BenjcoreUtil;
 
 public static class GetUtilVersion
 {
+    // Internal version identifiers
+    // When bumping the version, don't forget to update BenjcoreUtil.csproj.
+    private static uint[] _base_version => [2, 2, 0];
+    private static readonly uint? _preview_revision = 1;
+    private static readonly string? _preview_branch = "rc"; // Should be the branch's suffix.
+    
+    /// <summary>
+    /// BenjcoreUtil Version as a BenjcoreUtil <see cref="PreviewVersion"/> object.
+    /// </summary>
+    public static PreviewVersion VersionAsPreviewVersion =>
+        new(new SimpleVersion(_base_version), _preview_revision, x => x.SingleOrDefault(b => b.Suffix == _preview_branch));
+    
+    /// <summary>
+    /// BenjcoreUtil Version as a BenjcoreUtil <see cref="SimpleVersion"/> object.
+    /// </summary>
+    public static SimpleVersion VersionAsSimpleVersion => VersionAsPreviewVersion.SimpleVersion;
+    
     /// <summary>
     /// BenjcoreUtil Version as an Int Array.
     /// </summary>
-    public static readonly uint[] VersionAsUIntArray = [2, 2, 0];
+    public static uint[] VersionAsUIntArray => _base_version;
     
     /// <summary>
     /// BenjcoreUtil Version as a String.
     /// </summary>
-    public static readonly string VersionAsString =
-        $"{VersionAsUIntArray[0]}.{VersionAsUIntArray[1]}.{VersionAsUIntArray[2]}";
+    /// <remarks>
+    /// If the current version is a preview version, only the base version will be returned.
+    /// e.g. "1.0.0-rc.1" will return "1.0.0".
+    /// </remarks>
+    public static string VersionAsString => VersionAsSimpleVersion.ToString();
     
     /// <summary>
     /// BenjcoreUtil Version as a BenjcoreUtil <see cref="Version"/> object.
@@ -27,9 +48,4 @@ public static class GetUtilVersion
 #pragma warning disable CS0618
     public static readonly Version VersionAsVersion = new(new Simple(VersionAsUIntArray));
 #pragma warning restore CS0618
-    
-    /// <summary>
-    /// BenjcoreUtil Version as a BenjcoreUtil <see cref="SimpleVersion"/> object.
-    /// </summary>
-    public static readonly SimpleVersion VersionAsSimpleVersion = new(VersionAsUIntArray);
 }
